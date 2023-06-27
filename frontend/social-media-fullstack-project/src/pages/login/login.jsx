ae4dc0a87f7bacebfc75fe2cbc56fe142f17b03f
Gustavo Henrique <ghenriquesouza11@gmail.com>
2023-06-27 09:29:11 -0300

    cria e estiliza a página de login

--- frontend/social-media-fullstack-project/src/pages/login/login.jsx
+++ frontend/social-media-fullstack-project/src/pages/login/login.jsx
@@ -3,7 +3,24 @@ import './login.scss';
 
 function Login () {
 	return (
-		<div className="login">Login</div>
+		<div className="login">
+			<div className='card'>
+				<div className='left'>
+					<h1>Hello World.</h1>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, quia totam. Corrupti ea cumque vero consequatur, velit veniam eligendi fuga, magni quia beatae dolore laudantium. Provident quam quis vel earum?</p>
+					<span>Dont&apos;t you have an account?</span>
+					<button>Register</button>
+				</div>
+				<div className='right'>
+					<h1>Login</h1>
+					<form>
+						<input type='text' placeholder='Username' />
+						<input type='password' placeholder='password' />
+						<button>Login</button>
+					</form>
+				</div>
+			</div>
+		</div>
 	);
 }
 
