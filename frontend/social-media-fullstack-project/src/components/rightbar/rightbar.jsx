0d30bb52e02b19c23b69cede4264a1ec7afd615e
Gustavo Henrique <ghenriquesouza11@gmail.com>
2023-06-27 14:39:17 -0300

    Estiliza a barra lateral da direita

--- frontend/social-media-fullstack-project/src/components/rightbar/rightbar.jsx
+++ frontend/social-media-fullstack-project/src/components/rightbar/rightbar.jsx
@@ -3,7 +3,87 @@ import './rightbar.scss';
 
 function RightBar () {
 	return (
-		<div className="leftbar">RightBar</div>
+		<div className="rightbar">
+			<div className="container">
+				<div className="item">
+					<span>Suggestions for you</span>
+					<div className="user">
+						<div className="userInfo">
+							<img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="user-photo" />
+							<span>Jane Doe</span>
+						</div>
+						<div className="buttons">
+							<button>follow</button>
+							<button>dismiss</button>
+						</div>
+					</div>
+					<div className="user">
+						<div className="userInfo">
+							<img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="user-photo" />
+							<span>Jane Doe</span>
+						</div>
+						<div className="buttons">
+							<button>follow</button>
+							<button>dismiss</button>
+						</div>
+					</div>
+				</div>
+				<div className="item">
+					<span>Latest Activities</span>
+					<div className="user">
+						<div className="userInfo">
+							<img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="user-photo" />
+							<p>
+								<span>Jane Doe</span> changed their cover picture
+							</p>
+						</div>
+						<span>1 min ago</span>
+					</div>
+					<div className="user">
+						<div className="userInfo">
+							<img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="user-photo" />
+							<p>
+								<span>Jane Doe</span> changed their cover picture
+							</p>
+						</div>
+						<span>1 min ago</span>
+					</div>
+					<div className="user">
+						<div className="userInfo">
+							<img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="user-photo" />
+							<p>
+								<span>Jane Doe</span> changed their cover picture
+							</p>
+						</div>
+						<span>1 min ago</span>
+					</div>
+				</div>
+				<div className="item">
+					<span>Online Friends</span>
+					<div className="user">
+						<div className="userInfo">
+							<img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="user-photo" />
+							<div className="online" />
+							<span>Jane Doe</span>
+						</div>
+					</div>
+					<div className="user">
+						<div className="userInfo">
+							<img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="user-photo" />
+							<div className="online" />
+							<span>Jane Doe</span>
+						</div>
+					</div>
+					<div className="user">
+						<div className="userInfo">
+							<img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="user-photo" />
+							<div className="online" />
+							<span>Jane Doe</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	);
 }
 
